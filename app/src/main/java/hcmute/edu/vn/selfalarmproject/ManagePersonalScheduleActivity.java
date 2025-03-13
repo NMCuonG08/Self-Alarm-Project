aa8852c54ab2cbf45797e45b1aa576f594306442
lethinhute <thinhthcsah11@gmail.com>
2025-03-13 12:39:31 +0700

    initial schedule commit

---
+++ app/src/main/java/hcmute/edu/vn/selfalarmproject/ManagePersonalScheduleActivity.java
@@ -0,0 +1,12 @@
+package hcmute.edu.vn.selfalarmproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManagePersonalScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manage_personal_schedule); // Link XML layout
+    }
+}

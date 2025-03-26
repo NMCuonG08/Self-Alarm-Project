c0a63a64f64440339a91558b90cb9a7d729aa2b1
lethinhute <thinhthcsah11@gmail.com>
2025-03-26 22:23:19 +0700

    them chuc nang

---
+++ app/src/main/java/hcmute/edu/vn/selfalarmproject/AddEventActivity.java
@@ -0,0 +1,12 @@
+package hcmute.edu.vn.selfalarmproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEventActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event); // Ensure this XML file exists
+    }
+}
---
+++ app/src/main/java/hcmute/edu/vn/selfalarmproject/ViewScheduleActivity.java
@@ -0,0 +1,12 @@
+package hcmute.edu.vn.selfalarmproject;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_schedule); // Ensure this XML exists
+    }
+}
